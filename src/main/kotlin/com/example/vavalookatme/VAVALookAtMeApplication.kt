00583ad639c5102492fe38f65feca6692369ed85
taeyang121096 <taeyang121096@naver.com>
2025-07-11 16:41:12 +0900

    Initia project

---
+++ src/main/kotlin/com/example/vavalookatme/VAVALookAtMeApplication.kt
@@ -0,0 +1,11 @@
+package com.example.vavalookatme
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class VAVALookAtMeApplication
+
+fun main(args: Array<String>) {
+	runApplication<VAVALookAtMeApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/vavalookatme/VavaLookatmeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.vavalookatme
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class VavaLookatmeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
